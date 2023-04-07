5287e35b68152a8a451649d6fc161e9c0677c14e
Hyun Seok Shin <hskshin@umich.edu>
2023-04-07 10:41:34 -0400

    add debug print

--- dpvo/net.py
+++ dpvo/net.py
@@ -132,6 +132,11 @@ class Patchifier(nn.Module):
             x = torch.randint(1, w-1, size=[n, patches_per_image], device="cuda")
             y = torch.randint(1, h-1, size=[n, patches_per_image], device="cuda")
         
+        print("size of x: (line 135 of net.py)")
+        print(x.size())
+        print("size of y: (line 135 of net.py)")
+        print(y.size())
+        
         coords = torch.stack([x, y], dim=-1).float()
         imap = altcorr.patchify(imap[0], coords, 0).view(b, -1, DIM, 1, 1)
         gmap = altcorr.patchify(fmap[0], coords, P//2).view(b, -1, 128, P, P)
